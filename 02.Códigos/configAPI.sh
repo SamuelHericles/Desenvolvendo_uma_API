03cefbb42d4b117e83474b0e6ed6430d2f009d78
Lucas Costa Santos <lucas26xd@gmail.com>
2020-08-22 18:47:21 -0300

    Update configAPI.sh

--- 02.Códigos/configAPI.sh
+++ 02.Códigos/configAPI.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
 sudo apt-get update
+# Instala o Python, Pip, Nginx e Gunicorn
 sudo apt-get --assume-yes install python3 python3-pip nginx gunicorn3
+# Instala dependêcias da API
 sudo pip3 install flask flask_restful pandas xlrd
 
+# Baixa o código-fonte da API e arquivo de config do Nginx
 wget https://raw.githubusercontent.com/SamuelHericles/Desenvolvendo_uma_API/master/02.C%C3%B3digos/app.py
 wget https://raw.githubusercontent.com/SamuelHericles/Desenvolvendo_uma_API/master/02.C%C3%B3digos/api_settings
+
+# Move a nova config e remove a antiga
 sudo mv ./api_settings /etc/nginx/sites-enabled/
 sudo rm /etc/nginx/sites-enabled/default
-sudo service nginx restart
 
+# Restarta o serviço do Nginx
+sudo service nginx restart
+#Roda o script da API
 gunicorn3 app:app
